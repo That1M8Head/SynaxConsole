22e2670a165454c4035f202bec0e5917d83393e7
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-06-05 15:44:47 +0100

    Edit startup message

--- startup.h
+++ startup.h
@@ -6,5 +6,4 @@ startup.h - Holds the startup message for Synax Console
 
 static const char* SYN_StartupMessage = 
     "Welcome to Synax Console!\n"
-    "\n"
     "This is a work in progress command shell for *nix systems.\n";
