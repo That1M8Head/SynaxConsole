169e41c7ec7d53ca3cdb497fc82b1e87604c5b3d
Arsalan Kazmi <kaz18001436@student.westherts.ac.uk>
2021-10-08 15:52:20 +0100

    Added stuff, but I cannot figure out how this stupid thing works, this sucks

--- main.c
+++ main.c
@@ -17,15 +17,12 @@ main.c - Main source file for Synax Console
 /* Called for custom exceptions */
 void Error(int FatalErr, char* ErrMessage)
 {
-    if (FatalErr)
-    {
+    if (FatalErr) {
         /* Triggered on a fatal error */
         printf("Fatal: %s\n", ErrMessage);
-        printf("Exiting.\n");
+        puts("Exiting.");
         exit(EXIT_FAILURE);
-    }
-    else
-    {
+    } else {
         /* Triggered on a recoverable error */
         printf("Error: %s\n", ErrMessage);
     }
@@ -34,12 +31,30 @@ void Error(int FatalErr, char* ErrMessage)
 int main(int argc, char* argv[])
 {
     /* Startup message */
-    printf("Starting Synax Console\n");
-    printf(SYN_StartupMessage);
+    printf("%s\n", SYN_StartupMessage);
 
     /* Variable declaration */
-    int LoggedIn;
-    char* Prompt;
-    char path[200];
-    char* CurrentWorkingDirectory = getcwd(path, 200);
+    int LoggedIn = 1;
+    char Path[200];
+    char* Command = "";
+    char* CurrentWorkingDirectory = getcwd(Path, 200);
+
+    /* Main loop */
+    while (LoggedIn)
+    {
+	/* Shows prompt and handles input */
+	printf("%s\\", getlogin()); printf("%s\\", CurrentWorkingDirectory); printf("synax\n> ");
+	fgets(Command, 100, stdin);
+	Command[strcspn(Command, "\n")] = 0;
+
+	/* Exit command */
+	if (!strcmp(Command, "exit")) {
+	    LoggedIn = 0;
+	}
+
+	/* Tell the user they can't do anything and they need to exit because I didn't code in functionality yet lol */
+	puts("Command processor not implemented. (yet)\nType 'exit' to exit.");
+    }
+    puts("Exiting.");
+    return 0;
 }
