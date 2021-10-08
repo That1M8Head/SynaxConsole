6acb35dddaa99c5fa52a34d0fc41f909375ddc28
Arsalan Kazmi <kaz18001436@student.westherts.ac.uk>
2021-10-08 15:08:38 +0100

    Replace build script with makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+synax-console:
+	gcc -o sycn main.c -Wall
+install:
+	cp sycn /usr/local/bin
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Build script written by Arsalan Kazmi, but really it could have been written by anyone ¯\_(ツ)_/¯
-
-echo Building Synax Console with GCC...
-gcc -o sycn main.c -Wall
